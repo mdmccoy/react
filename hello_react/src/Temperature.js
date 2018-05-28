d322f4b178cd292087d9ecd5faf829d85ad84be3
Matt McCoy <mdmccoy@gmail.com>
2018-05-28 09:10:46 -0400

    Added form to take in city.

--- hello_react/src/Temperature.js
+++ hello_react/src/Temperature.js
@@ -4,20 +4,45 @@ import './Temperature.css'
 
 
 // class Temperature extends React.Component{
+//     // constructor(){
+//     //     super()
+//     //     this.state =  {city: 'Delray Beach, FL'}
+//     // }
+
+
+
 //     render(){
 //         return(
-//             <span className="temperature">
-//             {this.props.value}
-//             </span>
+//             <div>
+//                 <form onSubmit={this.props.onSubmit}>
+//                     <label htmlFor="city">City:</label>
+//                     <input type="text" id="city" onChange={this.props.onChange} placeholder="Delray Beach, FL" />
+//                     <input type="submit" value="Submit" />
+//                 </form>
+            
+
+//                 <span className="temperature">
+//                 {this.props.value}
+//                 </span>
+//             </div>
 //         )
 //     }
 // }
 
 var Temperature = (props) => {
     return (
-        <span className="temperature">
-        {props.temperature.value}{props.temperature.unit}
-        </span>
+            <div>
+                 <form onSubmit={props.onSubmit}>
+                     <label htmlFor="city">City:</label>
+                     <input type="text" id="city" onChange={props.onChange} placeholder="Delray Beach, FL" />
+                     <input type="submit" value="Submit" />
+                 </form>
+            
+
+                 <span className="temperature">
+                 {props.value}{props.unit}
+                 </span>
+             </div>
     )
 }
 
--- hello_react/src/TemperatureContainer.js
+++ hello_react/src/TemperatureContainer.js
@@ -4,26 +4,45 @@ import Temperature from './Temperature'
 class TemperatureContainer extends React.Component{
     constructor(){
         super();
-        this.state = {value:0}
+        this.state = {value:0, city: "Delray Berach, FL"}
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     componentDidMount() {
-        fetch('/weather')
+        this.updateWeather()        
+    }
+
+    updateWeather(){
+        console.log(`weather for ${this.state.city}`);
+        fetch(`/weather?city=${this.state.city}`)
+        .then(response => response.json())
         .then(
-            (response) => response.json()
-        ).then(
             (jsonResponse) => {
                 this.setState({
-                    value:jsonResponse["main"]["temp"],
-                    unit:'F'
+                    value:jsonResponse["main"]["temp"]                    
                 })
             }
-        )
+        )        
+    }
+
+
+    handleChange(event){
+        this.setState({
+            city: event.target.value
+        });
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        this.updateWeather();
+        // console.log('submit',this.state.city);        
     }
     
     render(){
         return(
-            <Temperature temperature={this.state} />
+            <Temperature value={this.state.value} onChange={this.handleChange} onSubmit={this.handleSubmit} />
         )
     }
 }
